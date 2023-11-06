b09aa24e62a862d4d0297e03d99b861c1144bf75
AmbrishProject <ambreesh09tripathi@gmail.com>
2023-11-06 11:47:21 +0530

    wa

--- lib/view/main/components/connect_button.dart
+++ lib/view/main/components/connect_button.dart
@@ -12,7 +12,7 @@ class ConnectButton extends StatelessWidget {
       padding:  const EdgeInsets.symmetric(vertical: defaultPadding),
       child: InkWell(
         onTap: () {
-          // launchUrl(Uri.parse('https://wa.me/03054200605'));
+          launchUrl(Uri.parse('https://wa.link/rxmt80'));
         },
         borderRadius: BorderRadius.circular(defaultPadding +10),
         child: Container(
